802adf520b30b1de945fcdb542152bb19ad4cb45
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2012-10-28 18:04:49 +0900

    とりあえず動作する

--- excale-socketio/ViewController.h
+++ excale-socketio/ViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ViewController : UIViewController <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *inputField;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
 @end
--- excale-socketio/ViewController.m
+++ excale-socketio/ViewController.m
@@ -7,17 +7,31 @@
 //
 
 #import "ViewController.h"
+#import "AZSocketIO.h"
+
+#define SOCKETIO_HOST @"niconicoar.excale.net"
+//#define SOCKETIO_HOST @"niconicoar.heroku.com"
+#define SOCKETIO_PORT @"80"
+#define BROADCAST_EVENT @"broadcast"
+#define SEND_EVENT @"message"
 
 @interface ViewController ()
-
+- (void)setUpSocketIO;
+- (void)receiveMessage:(NSString *)message;
+- (void)sendMessage:(NSString *)message;
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    __strong AZSocketIO *_socketIO;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.inputField.returnKeyType = UIReturnKeySend;
+    self.inputField.delegate = self;
+    self.textView.text = @"";
+    [self setUpSocketIO];
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +40,55 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)viewDidUnload {
+    [self setTextView:nil];
+    [self setInputField:nil];
+    [super viewDidUnload];
+}
+
+#pragma mark - UITextFieldDelegate
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    NSLog(@"textFieldShouldReturn:");
+    [self sendMessage:self.inputField.text];
+    self.inputField.text = @"";
+    [self.inputField resignFirstResponder];
+    return YES;
+}
+
+#pragma mark - private method
+- (void)setUpSocketIO
+{
+    NSLog(@"setUpSocketIO");
+    _socketIO = [[AZSocketIO alloc] initWithHost:SOCKETIO_HOST andPort:nil secure:NO];
+    _socketIO.transports = [NSMutableSet setWithObject:@"xhr-polling"];
+    __block ViewController *blockSelf = self;
+    [_socketIO setEventRecievedBlock:^(NSString *eventName, id data){
+        if ([eventName isEqualToString:BROADCAST_EVENT]) {
+            [blockSelf receiveMessage:[data objectAtIndex:0]];
+        }
+    }];
+    [_socketIO connectWithSuccess:^{
+        NSLog(@"connection success");
+    } andFailure:^(NSError *error){
+        NSLog(@"connection fail:%@", [error description]);
+    }];
+}
+
+- (void)receiveMessage:(NSString *)message
+{
+    NSLog(@"received message:%@", message);
+    self.textView.text = [self.textView.text stringByAppendingFormat:@"\n%@", message];
+}
+
+- (void)sendMessage:(NSString *)message
+{
+    NSLog(@"send message:%@", message);
+    NSError *error;
+    NSArray *args = [NSArray arrayWithObject:message];
+    [_socketIO emit:SEND_EVENT args:args error:&error];
+    if (error) {
+        NSLog(@"send message fail:%@", [error description]);
+    }
+}
 @end
